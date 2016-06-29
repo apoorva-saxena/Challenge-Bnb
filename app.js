02c15a824080310fd983860f488d6c593545065c
Apoorva Saxena <apsaxena24@gmail.com>
2016-06-29 21:24:28 +0100

    Second test and sign up page added

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.set('view engine', 'ejs');
 app.get("/users/new", function(req, res) {
   res.render("users/new");
 });
+app.post("/users", function(req, res) {
+  res.direct('/');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- spec/features/userManagementSpec.js
+++ spec/features/userManagementSpec.js
@@ -1,7 +1,17 @@
-casper.test.begin("Locate the first page", 1, function(test) {
-  casper.start('http://localhost:3000/users/new', function() {
-    test.assertSelectorExists("h1", "Please sign up");
-  }).run(function() {
+casper.test.begin('Sign Up page', 2, function(test) {
+    casper.start('http://localhost:3000/users/new', function() {
+        test.assertSelectorExists("h1", "Please sign up");
+
+        this.fill('form[action="/users"]', {
+          email: "test@test.com",
+          name: "Apoorva",
+          password: "secret"
+        });
+        this.click("[name='signup']");
+        this.echo("redirected to new page");
+        test.assertExists("h1", "Hello");
+        
+    }).run(function() {
         test.done();
     });
 });
